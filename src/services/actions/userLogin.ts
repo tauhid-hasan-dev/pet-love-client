fb642d63edad2f5ed91c0675a9de35b4ec83ade2
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-23 13:07:43 +0300

    user logged in successfully

--- src/services/actions/userLogin.ts
+++ src/services/actions/userLogin.ts
@@ -1,9 +1,9 @@
 "use server";
-import { FormValues } from "@/app/login/page";
+import { FieldValues } from "react-hook-form";
 
 // ${process.env.NEXT_PUBLIC_BACKEND_API_URL}
 
-export const userLogin = async (data: FormValues) => {
+export const userLogin = async (data: FieldValues) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/login`, {
     method: "POST",
     headers: {
@@ -15,3 +15,35 @@ export const userLogin = async (data: FormValues) => {
   const userInfo = await res.json();
   return userInfo;
 };
+
+// "use server";
+
+/* import { FieldValues } from "react-hook-form";
+import setAccessToken from "./setAccessToken";
+
+export const userLogin = async (data: FieldValues) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      credentials: "include",
+      // cache: "no-store",
+    }
+  );
+  const userInfo = await res.json();
+
+  const passwordChangeRequired = userInfo.data.needPasswordChange;
+
+  if (userInfo.data.accessToken) {
+    setAccessToken(userInfo.data.accessToken, {
+      redirect: "/dashboard",
+      passwordChangeRequired,
+    });
+  }
+
+  return userInfo;
+}; */
