695ec9013d5587018cca9061981d65a7036f1dc6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-24 11:10:56 +0300

    adding token to the request

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -1,8 +1,39 @@
+import { authKey } from "@/app/constants/authKey";
+import { getFromLocalStorage } from "@/utils/local-storage";
 import axios from "axios";
 
 const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accept"] = "application/json";
 instance.defaults.timeout = 60000;
+
+// We simply adding the access token within our request to the backend
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getFromLocalStorage(authKey);
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 export { instance };
