0f3d7ce1066c4aab962f57fc8fd123494debbb18
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-24 11:04:16 +0300

    default axios instance added to the axios base query

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -2,7 +2,8 @@ import { IMeta } from "@/types";
 import type { BaseQueryFn } from "@reduxjs/toolkit/query";
 
 import type { AxiosRequestConfig, AxiosError } from "axios";
-import axios from "axios";
+
+import { instance as axiosInstance } from "./axiosInstance";
 
 export const axiosBaseQuery =
   (
@@ -22,7 +23,7 @@ export const axiosBaseQuery =
   > =>
   async ({ url, method, data, params, headers, contentType }) => {
     try {
-      const result = await axios({
+      const result = await axiosInstance({
         url: baseUrl + url,
         method,
         data,
