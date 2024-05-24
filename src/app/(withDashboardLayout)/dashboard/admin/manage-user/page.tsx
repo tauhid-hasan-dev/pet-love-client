d162f0a6d1b59bbddf917be664b61407afb8d352
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-24 19:08:29 +0300

    basic data grid applied for user listing

--- src/app/(withDashboardLayout)/dashboard/admin/manage-user/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-user/page.tsx
@@ -1,11 +1,63 @@
 "use client";
 
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
 import PetModal from "./components/PetModal";
 import { useState } from "react";
+import { useGetAllUserQuery } from "@/redux/api/userApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const ManageUsers = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const { data, isLoading } = useGetAllUserQuery({});
+  console.log({ data });
+
+  /* const handleDelete = async (id: string) => {
+    try {
+      const res = await deleteSpecialty(id).unwrap();
+      if (res?.id) {
+        toast.success("Specialty deleted successfully!!!");
+      }
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }; */
+
+  // console.log(data);
+  const columns: GridColDef[] = [
+    {
+      field: "icon",
+      headerName: "Icon",
+
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Image src={row.profilePhoto} width={30} height={30} alt="icon" />
+          </Box>
+        );
+      },
+    },
+    { field: "name", headerName: "Name", width: 400 },
+    { field: "email", headerName: "Email", width: 400 },
+
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <IconButton
+            /* onClick={() => handleDelete(row.id)}  */ aria-label="delete"
+          >
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between">
@@ -13,6 +65,13 @@ const ManageUsers = () => {
         <PetModal open={isModalOpen} setOpen={setIsModalOpen}></PetModal>
         <TextField size="small" placeholder="Search users"></TextField>
       </Stack>
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={data} columns={columns} hideFooter={true} />
+        </Box>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
     </Box>
   );
 };
