7b1339b1160896aad8011d0f7c9938855a403a1d
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-06-02 15:32:18 +0300

    see pet details button created

--- src/app/(withDashboardLayout)/dashboard/user/adoption-requests/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/adoption-requests/page.tsx
@@ -2,6 +2,8 @@
 
 import { useGetPendingAdoptionRequestsQuery } from "@/redux/api/adoptionRequestApi";
 import { getUserInfo } from "@/services/auth.services";
+import { Box, Button, CircularProgress } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 
 type TUserInfo = {
@@ -23,13 +25,55 @@ const AdoptionRequests = () => {
     }
   }, []);
 
-  const { data } = useGetPendingAdoptionRequestsQuery(user?.id);
-  console.log({ data });
+  const { data: pendingRequests, isLoading } =
+    useGetPendingAdoptionRequestsQuery(user?.id);
+  /* console.log(pendingRequests); */
+
+  const handleShowPetDetails = async (id: string) => {
+    console.log(id);
+  };
+
+  const columns: GridColDef[] = [
+    { field: "userName", headerName: "User Name", flex: 2 },
+    { field: "userEmail", headerName: "User Email", flex: 2 },
+    { field: "status", headerName: "Request Status", flex: 2 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={() => handleShowPetDetails(row?.id)}
+          >
+            See Pet Details
+          </Button>
+        );
+      },
+    },
+  ];
 
   return (
-    <div>
-      <h1>My all adoption requests list</h1>
-    </div>
+    <Box>
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={pendingRequests} columns={columns} />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "calc(100vh - 100px)",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
+    </Box>
   );
 };
 
