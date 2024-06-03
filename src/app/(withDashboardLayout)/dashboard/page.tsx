a3c62f0badaeb58a8ccd7349d37328b82fdee1ec
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-06-03 21:56:04 +0300

    card added to the user dashboard

--- src/app/(withDashboardLayout)/dashboard/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -1,19 +1,8 @@
-import { Card, CardContent, Stack, Typography } from "@mui/material";
-
 const DashboardHomePage = () => {
   return (
-    <Stack>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
-        </CardContent>
-      </Card>
-    </Stack>
+    <div>
+      <h1>Welcome to dashboard</h1>
+    </div>
   );
 };
 
--- src/app/(withDashboardLayout)/dashboard/user/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/page.tsx
@@ -1,10 +1,59 @@
-import React from "react";
+import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
+import PetsIcon from "@mui/icons-material/Pets";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const UserDashboard = () => {
   return (
-    <div>
-      <h1>This is user dashboard</h1>
-    </div>
+    <Stack direction="row" spacing={4}>
+      <Card sx={{ minWidth: 400, p: 2, borderRadius: 4 }}>
+        <CardContent>
+          <Stack direction="row" justifyContent="space-between">
+            <Box>
+              <Typography
+                sx={{ fontSize: 17 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Total Adopted Pets
+              </Typography>
+              <Typography
+                sx={{ fontSize: 36, color: "#6504B5", fontWeight: "bold" }}
+                color="text.secondary"
+                gutterBottom
+              >
+                14
+              </Typography>
+            </Box>
+            <PetsIcon sx={{ fontSize: 45, color: "#F1EAFF" }}></PetsIcon>
+          </Stack>
+        </CardContent>
+      </Card>
+      <Card sx={{ minWidth: 400, p: 2, borderRadius: 4 }}>
+        <CardContent>
+          <Stack direction="row" justifyContent="space-between">
+            <Box>
+              <Typography
+                sx={{ fontSize: 17 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Total Adoption Requests
+              </Typography>
+              <Typography
+                sx={{ fontSize: 36, color: "#6504B5", fontWeight: "bold" }}
+                color="text.secondary"
+                gutterBottom
+              >
+                14
+              </Typography>
+            </Box>
+            <MailOutlineIcon
+              sx={{ fontSize: 45, color: "#F1EAFF" }}
+            ></MailOutlineIcon>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
   );
 };
 
