fc1d8868a98370f52f7fb26e67d5cfa8bbcc153c
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-06-03 23:19:42 +0300

    user dashboard card updated

--- src/app/(withDashboardLayout)/dashboard/user/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/page.tsx
@@ -1,8 +1,37 @@
+"use client";
+
 import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
 import PetsIcon from "@mui/icons-material/Pets";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import {
+  useGetAllAdoptedPetsQuery,
+  useGetPendingAdoptionRequestsQuery,
+} from "@/redux/api/adoptionRequestApi";
+import { useEffect, useState } from "react";
+import { getUserInfo } from "@/services/auth.services";
+
+type TUserInfo = {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  iat: number;
+  exp: number;
+};
 
 const UserDashboard = () => {
+  const [user, setUser] = useState<TUserInfo | null>(null);
+
+  useEffect(() => {
+    const userInfo = getUserInfo();
+    if (userInfo) {
+      setUser(userInfo);
+    }
+  }, []);
+
+  const { data: adoptedPets } = useGetAllAdoptedPetsQuery(user?.id);
+  const { data } = useGetPendingAdoptionRequestsQuery(user?.id);
+  console.log(data);
   return (
     <Stack direction="row" spacing={4}>
       <Card sx={{ minWidth: 400, p: 2, borderRadius: 4 }}>
@@ -21,7 +50,7 @@ const UserDashboard = () => {
                 color="text.secondary"
                 gutterBottom
               >
-                14
+                {adoptedPets?.length}
               </Typography>
             </Box>
             <PetsIcon sx={{ fontSize: 45, color: "#F1EAFF" }}></PetsIcon>
@@ -37,14 +66,14 @@ const UserDashboard = () => {
                 color="text.secondary"
                 gutterBottom
               >
-                Total Adoption Requests
+                Total Pending Adoption Requests
               </Typography>
               <Typography
                 sx={{ fontSize: 36, color: "#6504B5", fontWeight: "bold" }}
                 color="text.secondary"
                 gutterBottom
               >
-                14
+                {data?.length}
               </Typography>
             </Box>
             <MailOutlineIcon
