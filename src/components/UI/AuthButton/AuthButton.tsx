c02f04a21c0d3729c867198510d134e976bfb136
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-27 19:13:27 +0300

    responsiveness achieved

--- src/components/UI/AuthButton/AuthButton.tsx
+++ src/components/UI/AuthButton/AuthButton.tsx
@@ -1,22 +1,25 @@
 import { logoutUser } from "@/services/actions/logutUser";
-import { getUserInfo, removeUser } from "@/services/auth.services";
+import { getUserInfo } from "@/services/auth.services";
 import { Button, Stack } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const AuthButton = () => {
   const userInfo = getUserInfo();
   const router = useRouter();
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   const handleLogOut = () => {
     logoutUser(router);
   };
+
   return (
     <>
       {userInfo?.id ? (
         <Button onClick={handleLogOut}>Logout</Button>
       ) : (
-        <Stack direction="row" gap={2}>
+        <Stack direction={isMobile ? "column" : "row"} gap={2}>
           <Button component={Link} href="/login">
             Login
           </Button>
